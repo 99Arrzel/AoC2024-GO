a59e369da30d985aca2fbe3a77c7ddc4a3c427c4
Andres Carrillo <af.carrillo@live.com>
2024-12-07 00:01:51 -0400

    Part 2

--- day1.go
+++ day1.go
@@ -17,20 +17,35 @@ func check(e error) {
 
 func main() {
 	//Read
+	// dat, err := os.ReadFile("./test1_1.txt")
 	dat, err := os.ReadFile("./input1_1.txt")
 	check(err)
 
 	stringValue := string(dat)
 	rowValues := strings.Split(stringValue, "\n")
-	firstColumn := make([]int32, 0)
-	secondColumn := make([]int32, 0)
+	firstColumn := make([]int, 0)
+	secondColumn := make([]int, 0)
+	firstColumnMap := make(map[int]int)
+	secondColumnMap := make(map[int]int)
 	for _, rowValue := range rowValues {
 		values := strings.Split(rowValue, "   ")
 		firstValue, err := strconv.Atoi(values[0])
 		secondValue, err := strconv.Atoi(values[1])
+		_, firstMapOk := firstColumnMap[firstValue]
+		_, secondMapOk := secondColumnMap[secondValue]
+		if firstMapOk {
+			firstColumnMap[firstValue]++
+		} else {
+			firstColumnMap[firstValue] = 1
+		}
+		if secondMapOk {
+			secondColumnMap[secondValue]++
+		} else {
+			secondColumnMap[secondValue] = 1
+		}
 		check(err)
-		firstColumn = append(firstColumn, int32(firstValue))
-		secondColumn = append(secondColumn, int32(secondValue))
+		firstColumn = append(firstColumn, firstValue)
+		secondColumn = append(secondColumn, secondValue)
 	}
 	//Sort
 	slices.Sort(firstColumn)
@@ -41,4 +56,15 @@ func main() {
 		totalDiff = totalDiff + int(math.Abs(float64(firstColumn[i])-float64(secondColumn[i])))
 	}
 	fmt.Println(totalDiff)
+	//part 2
+	similarity := 0
+	for i := 0; i < len(firstColumn); i++ {
+		multiplier := 0
+		valFound, found := secondColumnMap[firstColumn[i]] 
+		if found {
+			multiplier = valFound			
+		}			
+		similarity = similarity + multiplier * firstColumn[i]
+	}
+	fmt.Println(similarity)
 }
