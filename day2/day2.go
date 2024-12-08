690191bd8e9c7aaa0e8e51b7c8bad4b914081bfe
Andres Carrillo <af.carrillo@live.com>
2024-12-07 20:19:03 -0400

    Day 2 part 2 working

--- day2/day2.go
+++ day2/day2.go
@@ -13,53 +13,87 @@ func safeRange(x int, y int) (result bool) {
 	if x == y {
 		return false
 	}
-	if (int(math.Abs(float64(x - y))) > maxGap) {
+	if int(math.Abs(float64(x-y))) > maxGap {
 		return false
 	}
 	return true
 }
+func removeAndNew(slice []int, s int) []int {
+	newSlice := make([]int, 0)
+	for idx := 0; idx < len(slice); idx++ {
+		if idx == s {
+			continue
+		}
+		newSlice = append(newSlice, slice[idx])
+	}
+	return newSlice
+}
+
+// Must be of at least lenght 2
+func evaluateRowSafeness(values []int) bool {
+	arrLen := len(values)
+	if arrLen < 2 {
+		return true
+	}
+	firstValue := values[0]
+	secondValue := values[1]
+	increasingOrDecreasing := "increasing"
+	if firstValue > secondValue {
+		increasingOrDecreasing = "decreasing"
+	}
+	for idx := 0; idx < arrLen; idx++ {
+		intValue := values[idx]
+		if idx > 0 {
+			oldIntValue := values[idx-1]
+			if !safeRange(oldIntValue, intValue) {
+				fmt.Println("Not safe by range", oldIntValue, intValue)
+				return false
+			} else if increasingOrDecreasing == "increasing" && oldIntValue > intValue {
+				fmt.Println("Not safe due to change of increase", oldIntValue, intValue)
+				return false
+			} else if increasingOrDecreasing == "decreasing" && intValue > oldIntValue {
+				fmt.Println("Not safe due to change of decrease", oldIntValue, intValue)
+				return false
+			}
+		}
+	}
+	return true
+}
+func toIntSlice(values []string) []int {
+	newSlice := make([]int, 0)
+	for idx := 0; idx < len(values); idx++ {
+		intVal, _ := strconv.Atoi(values[idx])
+		newSlice = append(newSlice, intVal)
+	}
+	return newSlice
+}
 
 func main() {
 	//Read
-	// dat, err := os.ReadFile("./test2_1.txt")
+	// dat, _ := os.ReadFile("./test2_1.txt")
 	dat, _ := os.ReadFile("./input2_1.txt")
 	stringValue := string(dat)
 	rowValues := strings.Split(stringValue, "\n")
 	safeCounter := 0
-	for _, rowValue := range rowValues {
-		values := strings.Split(rowValue, " ")
-		isSafe := true
-		firstValue, _ := strconv.Atoi(values[0])
-		secondValue, _ := strconv.Atoi(values[1])
-		increasingOrDecreasing := "increasing"
-		if firstValue == secondValue {
-			continue
-		}
-		if firstValue > secondValue {
-			increasingOrDecreasing = "decreasing"
-		}
 
-		for idx, value := range values {
-			intValue, _ := strconv.Atoi(value)
-			if idx > 0 {
-				oldIntValue, _ := strconv.Atoi(values[idx-1])
-				if !safeRange(oldIntValue, intValue) {
-					fmt.Println("UNSAFE", intValue, oldIntValue)
-					isSafe = false
-				}
-				if increasingOrDecreasing == "increasing" && oldIntValue > intValue {
-					isSafe = false
-					break
-				}
-				if increasingOrDecreasing == "decreasing" && intValue > oldIntValue {
-					isSafe = false
+	for _, rowValue := range rowValues {
+		values := toIntSlice(strings.Split(rowValue, " "))
+		isSafe := evaluateRowSafeness(values)
+		if isSafe {
+			safeCounter++
+		} else {
+			//Try removing index positions
+			for idx := 0; idx < len(values); idx++ {
+				newSlice := removeAndNew(values, idx)
+				fmt.Println(newSlice)
+				isReallySafe := evaluateRowSafeness(newSlice)
+				if isReallySafe {
+					safeCounter++
 					break
 				}
 			}
 		}
-		if isSafe {
-			safeCounter++
-		}
+
 	}
-	fmt.Println(safeCounter)
+	fmt.Println(safeCounter, "Number of safe rows")
 }
