b74440dab78f005fa636c80b1688bfdf5364bab4
Andres Carrillo <af.carrillo@live.com>
2024-12-09 01:25:22 -0400

    Hardcoded step 2, MAS LIST

--- day4/day4.go
+++ day4/day4.go
@@ -53,6 +53,23 @@ func countInArray(input []string, search string) int {
 	}
 	return total
 }
+func evaluateMAS(input string) bool {
+	validSequences := []string{"MSMS", "SMMS", "SMSM", "MSSM"}
+	for x := 0; x < len(validSequences); x++ {
+		if strings.Contains(input, validSequences[x]) {
+			return true
+		}
+	}
+	return false
+}
+func appendIfUnique(input []string, search string) []string {
+	for x := 0; x < len(input); x++ {
+		if input[x] == search {
+			return input
+		}
+	}
+	return append(input, search)
+}
 func diagonalsSearch(input [][]string, search string) ([]string, int) {
 	total := 0
 	completeList := make([]string, 0)
@@ -60,13 +77,41 @@ func diagonalsSearch(input [][]string, search string) ([]string, int) {
 	bottomLeftList := make([]string, 0)
 	topLeftList := make([]string, 0)
 	bottomRightList := make([]string, 0)
+	MASList := make([]string, 0)
 	for z := 0; z < len(input); z++ {
 		topRightAccumulator := ""
 		bottomLeftAccumulator := ""
 		topLeftAccumulator := ""
 		bottomRightAccumulator := ""
 		for x := 0; x < len(input); x++ {
+
 			for y := 0; y < len(input); y++ {
+				//Hardcoded MAS search
+				if input[x][y] == "A" {
+					coordinates := fmt.Sprintf("%d,%d", x, y)
+					//Search around and X
+					buffer := ""
+					if x-1 >= 0 && y-1 >= 0 && (input[x-1][y-1] == "M" || input[x-1][y-1] == "S") { //topLeft
+						buffer += input[x-1][y-1]
+					}
+					if x+1 < len(input) && y+1 < len(input) && (input[x+1][y+1] == "M" || input[x+1][y+1] == "S") { //bottomRight
+						buffer += input[x+1][y+1]
+					}
+					if x+1 < len(input) && y-1 >= 0 && (input[x+1][y-1] == "M" || input[x+1][y-1] == "S") { //topRight
+						buffer += input[x+1][y-1]
+					}
+					if x-1 >= 0 && y+1 < len(input) && (input[x-1][y+1] == "M" || input[x-1][y+1] == "S") { //bottomLeft
+						buffer += input[x-1][y+1]
+					}
+					if len(buffer) == 4 && z == 0 {
+						fmt.Println(buffer, coordinates)
+						isMas := evaluateMAS(buffer)
+						if isMas {
+							MASList = appendIfUnique(MASList, coordinates)
+						}
+					}
+				}
+
 				if x+y+z == len(input)-1 && z != 0 {
 					topLeftAccumulator += input[x][y]
 					// fmt.Print("👍")
@@ -98,23 +143,34 @@ func diagonalsSearch(input [][]string, search string) ([]string, int) {
 		bottomRightList = append(bottomRightList, bottomRightAccumulator)
 		// fmt.Println("////////")
 	}
-	completeList = append(completeList, topRightList...)
-	completeList = append(completeList, bottomLeftList...)
-	completeList = append(completeList, topLeftList...)
-	completeList = append(completeList, bottomRightList...)
+	// completeList = append(completeList, topRightList...)
+	// completeList = append(completeList, bottomLeftList...)
+	// completeList = append(completeList, topLeftList...)
+	// completeList = append(completeList, bottomRightList...)
+	fmt.Println(MASList, len(MASList), "MAS LIST")
+	bottomLeftToRightTop := make([]string, 0)
+	bottomLeftToRightTop = append(bottomLeftToRightTop, bottomLeftList...)
+	bottomLeftToRightTop = append(bottomLeftToRightTop, topRightList...)
+
+	topLeftToBottomRight := make([]string, 0)
+	topLeftToBottomRight = append(topLeftToBottomRight, topLeftList...)
+	topLeftToBottomRight = append(topLeftToBottomRight, bottomRightList...)
+
+
+	completeList = append(completeList, bottomLeftToRightTop...)
+	completeList = append(completeList, topLeftToBottomRight...)
 	for _, item := range completeList {
 		completeList = append(completeList, reverseWord(item))
 	}
 	total += countInArray(completeList, search)
-	fmt.Println("Total", total)
-	fmt.Println(completeList)
+	
 	return completeList, total // remove the always 2 dupplicates
 }
 func main() {
-	// dat, _ := os.ReadFile("./test4_1.txt")
+	// dat, _ := os.ReadFile("./test4_3.txt")
 	// dat, _ := os.ReadFile("./test3_2.txt")
 	dat, _ := os.ReadFile("./input4_1.txt")
-	searchWord := "XMAS"
+	searchWord := "MAS"
 	stringValue := string(dat)
 	matrixs := inputToMatrixs(stringValue)
 	total := 0
@@ -127,3 +183,4 @@ func main() {
 	_, total2 := diagonalsSearch(matrixs, searchWord)
 	fmt.Println(total + total2)
 }
+//> 1406
