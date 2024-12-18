c3cc2fcfaf070dd2ed96a00eca230f275a0c95e7
Andres Carrillo <af.carrillo@live.com>
2024-12-18 01:50:21 -0400

    WIP

--- day6/day6.go
+++ day6/day6.go
@@ -16,7 +16,6 @@ func main() {
 
 func WithInput(textMap []byte) {
 	mapLines := strings.Split(string(textMap), "\n")
-
 	maze := make([][]string, 0)
 	wardCoordinates := make([]int, 2)
 	wardFound := false
@@ -39,77 +38,103 @@ func WithInput(textMap []byte) {
 		fmt.Println("No ward")
 		return
 	}
-	// visitedPlaces := map[string]string{}
-	visitedPlaces := make([]string, 0)
-	direction := "top"
 	WardNextPlace := make([]int, 2)
 	WardNextPlace[0] = wardCoordinates[0]
 	WardNextPlace[1] = wardCoordinates[1]
-	mapPlaces := make(map[string]bool)
-	for WardInMap(wardCoordinates, mapLines) {
-		maze[wardCoordinates[0]][wardCoordinates[1]] = "X"
-		y := strconv.Itoa(wardCoordinates[1])
-		x := strconv.Itoa(wardCoordinates[0])
-		// visitedPlaces[x+y] = maze[wardCoordinates[0]][wardCoordinates[1]]
-		visitedPlaces = appendIfUnique(visitedPlaces, x+":"+y)
-		mapPlaces[x+":"+y] = true
+	uniquePlaces, infiniteLoops := evaluateUniquePlaces(wardCoordinates, WardNextPlace, maze)
+	fmt.Println("Total places", len(uniquePlaces))
+	fmt.Println("InfiniteLoops", infiniteLoops)
+}
 
+func evaluateUniquePlaces(wardCoordinates []int, WardNextPlace []int, maze [][]string) (map[string]bool, int) {
+	direction := "top"
+	mapPlaces := make(map[string]bool)
+	maxSize := len(maze)
+	infiniteLoopsCounter := 0
+	for WardInMap(wardCoordinates, maxSize) {
+		mapPlaces[makeKey(wardCoordinates[0], wardCoordinates[1])] = true
 		Move(WardNextPlace, direction)
-		isValidWardInMap := WardInMap(WardNextPlace, mapLines)
+		isValidWardInMap := WardInMap(WardNextPlace, len(maze))
 		if !isValidWardInMap {
 			fmt.Println("Ending in ", WardNextPlace)
 			break
 		}
-		nextPlace := maze[WardNextPlace[0]][WardNextPlace[1]]
+		// fmt.Println("Ward coordinates", wardCoordinates)
+		if isMazeInfiniteLoopWith(maze, wardCoordinates, direction, WardNextPlace) {
+			// printMap(maze, wardCoordinates, direction, WardNextPlace)
+			infiniteLoopsCounter++
+			// fmt.Println("VALID INFINTE LOOP")
+		}
 
+		nextPlace := maze[WardNextPlace[0]][WardNextPlace[1]]
 		if nextPlace == "#" {
 			direction = DirectionChange(direction)
-
 			WardNextPlace[0] = wardCoordinates[0]
 			WardNextPlace[1] = wardCoordinates[1]
-
 			Move(WardNextPlace, direction)
 		}
-
 		Move(wardCoordinates, direction)
-		// printMap(maze, wardCoordinates, direction)
-		y = strconv.Itoa(wardCoordinates[1])
-		x = strconv.Itoa(wardCoordinates[0])
-		// fmt.Printf("%s %s \n", x, y)
-		// visitedPlaces[x+y] = maze[wardCoordinates[0]][wardCoordinates[1]]
-		visitedPlaces = appendIfUnique(visitedPlaces, x+y)
-	}
 
-	printMap(maze, wardCoordinates, direction)
-	fmt.Println("Total places", len(visitedPlaces), "ending", wardCoordinates, "Map", len(mapPlaces))
+	}
+	return mapPlaces, infiniteLoopsCounter
 }
-func printMap(maze [][]string, wardCoordinates []int, direction string) {
-	buffer := "__Maze__\n"
-	for x, char := range maze {
-		for y, char2 := range char {
-			if wardCoordinates[0] == x && wardCoordinates[1] == y {
-				if direction == "top" {
-					buffer += "^"
-				}
-				if direction == "right" {
-					buffer += ">"
-				}
-				if direction == "bottom" {
-					buffer += "v"
-				}
-				if direction == "left" {
-					buffer += "<"
-				}
-			} else {
-				buffer += char2
+
+// Evaluate if, with current maze, and ward position, this is a infinite loop
+func isMazeInfiniteLoopWith(maze [][]string, startPosition []int, startDirection string, blockPosition []int) bool {
+
+	position := copyPosition(startPosition)
+	direction := string(startDirection)
+	// didTurn := 0
+	nextPosition := copyPosition(position)
+	countloop := 0
+
+	traversedPaths := make(map[string]bool)
+	for WardInMap(position, len(maze)) {
+
+		Move(nextPosition, direction)
+		isValidWardInMap := WardInMap(nextPosition, len(maze))
+		if !isValidWardInMap {
+			return false
+		}
+		nextPlace := maze[nextPosition[0]][nextPosition[1]]
+		if nextPlace == "#" || (nextPosition[0] == blockPosition[0] && nextPosition[1] == blockPosition[1]) {
+			direction = DirectionChange(direction)
+			nextPosition = copyPosition(position)
+			Move(nextPosition, direction)
+			accumulator, changes := thisDirectionChanges(maze, position, direction)
+			if traversedPaths[accumulator] {
+				return true
+			}
+			if changes {
+				traversedPaths[accumulator] = true
 			}
 		}
-		buffer += "\n"
+		Move(position, direction)
+		countloop++
 	}
-	buffer += "------\n"
-	fmt.Println(buffer)
+	// printMap(maze, position, direction, blockPosition)
+	// fmt.Println("Bye", didTurn)
+	return false
+}
+func thisDirectionChanges(maze [][]string, position []int, direction string) (string, bool) {
+	newPos := copyPosition(position)
+	accumulator := ""
+	for WardInMap(newPos, len(maze)) {
+		accumulator += makeKey(newPos[0], newPos[1])
+		if maze[newPos[0]][newPos[1]] == "#" {
+			return accumulator, true
+		}
+		Move(newPos, direction)
+	}
+	return accumulator, false
 }
 
+func copyPosition(position []int) []int {
+	newPos := make([]int, 2)
+	newPos[0] = position[0]
+	newPos[1] = position[1]
+	return newPos
+}
 func Move(wardCoordinates []int, direction string) {
 	if direction == "top" {
 		wardCoordinates[0]--
@@ -134,16 +159,50 @@ func DirectionChange(direction string) string {
 	}
 	return "top"
 }
-func WardInMap(wardCoordinates []int, mapLines []string) bool {
-	return (wardCoordinates[0] < len(mapLines) && wardCoordinates[0] >= 0) && (wardCoordinates[1] < len(mapLines[0]) && wardCoordinates[1] >= 0)
+
+// Maze is square
+func WardInMap(wardCoordinates []int, maxSize int) bool {
+	return (wardCoordinates[0] < maxSize && wardCoordinates[0] >= 0) && (wardCoordinates[1] < maxSize && wardCoordinates[1] >= 0)
 
 }
+func makeKey(a int, b int) string {
+	x := strconv.Itoa(a)
+	y := strconv.Itoa(b)
+	return "[" +x + ":" + y + "]"
+}
 
-func appendIfUnique(input []string, search string) []string {
-	for x := 0; x < len(input); x++ {
-		if input[x] == search {
-			return input
+func printMap(maze [][]string, wardCoordinates []int, direction string, obstacle []int) {
+	buffer := "__Maze__\n"
+	for x, char := range maze {
+		for y, char2 := range char {
+
+			if obstacle[0] == x && obstacle[1] == y {
+				buffer += "O"
+				continue
+			}
+			if wardCoordinates[0] == x && wardCoordinates[1] == y {
+				if direction == "top" {
+					buffer += "^"
+				}
+				if direction == "right" {
+					buffer += ">"
+				}
+				if direction == "bottom" {
+					buffer += "v"
+				}
+				if direction == "left" {
+					buffer += "<"
+				}
+				continue
+			}
+			if char2 == "^" {
+				buffer += "."
+				continue
+			}
+			buffer += char2
 		}
+		buffer += "\n"
 	}
-	return append(input, search)
+	buffer += "------\n"
+	fmt.Println(buffer)
 }
