46ee5a536d53a0b4da828fd651c2f76285227b11
Andres Carrillo <af.carrillo@live.com>
2024-12-19 23:44:57 -0400

    Its not 1713

--- day6/day6.go
+++ day6/day6.go
@@ -8,12 +8,13 @@ import (
 )
 
 func main() {
+	fmt.Println("Test")
+	textMap, _ := os.ReadFile("./test_map.txt")
+	WithInput(textMap)
+	fmt.Println("First")
 	textMapInput, _ := os.ReadFile("./input_map.txt")
 	WithInput(textMapInput)
-	// textMap, _ := os.ReadFile("./test_map.txt")
-	// WithInput(textMap)
 }
-
 func WithInput(textMap []byte) {
 	mapLines := strings.Split(string(textMap), "\n")
 	maze := make([][]string, 0)
@@ -38,15 +39,11 @@ func WithInput(textMap []byte) {
 		fmt.Println("No ward")
 		return
 	}
-	WardNextPlace := make([]int, 2)
-	WardNextPlace[0] = wardCoordinates[0]
-	WardNextPlace[1] = wardCoordinates[1]
-	uniquePlaces, infiniteLoops, infinityLoopPlaces := evaluateUniquePlaces(wardCoordinates, WardNextPlace, maze)
+	uniquePlaces, infiniteLoops, infinityLoopPlaces := evaluateUniquePlaces(wardCoordinates, maze)
 	fmt.Println("Total places", len(uniquePlaces))
 	fmt.Println("InfiniteLoops", infiniteLoops, len(infinityLoopPlaces))
 }
-
-func evaluateUniquePlaces(wardCoordinates []int, WardNextPlace []int, maze [][]string) (map[string]bool, int,map[string]bool ) {
+func evaluateUniquePlaces(wardCoordinates []int, maze [][]string) (map[string]bool, int, map[string]bool) {
 	direction := "top"
 	mapPlaces := make(map[string]bool)
 	infinityMapPlaces := make(map[string]bool)
@@ -54,83 +51,66 @@ func evaluateUniquePlaces(wardCoordinates []int, WardNextPlace []int, maze [][]s
 	infiniteLoopsCounter := 0
 	for WardInMap(wardCoordinates, maxSize) {
 		mapPlaces[makeKey(wardCoordinates[0], wardCoordinates[1])] = true
-		Move(WardNextPlace, direction)
-		isValidWardInMap := WardInMap(WardNextPlace, len(maze))
+		Move(wardCoordinates, direction)
+		isValidWardInMap := WardInMap(wardCoordinates, len(maze))
 		if !isValidWardInMap {
-			fmt.Println("Ending in ", WardNextPlace)
+			fmt.Println("Ending in ", wardCoordinates)
 			break
 		}
-		// fmt.Println("Ward coordinates", wardCoordinates)
-		if isMazeInfiniteLoopWith(maze, wardCoordinates, direction, WardNextPlace) {
-			// printMap(maze, wardCoordinates, direction, WardNextPlace)
-			infiniteLoopsCounter++
-			infinityMapPlaces[makeKey(WardNextPlace[0], WardNextPlace[1])] = true
-			// fmt.Println("VALID INFINTE LOOP")
-		}
-
-		nextPlace := maze[WardNextPlace[0]][WardNextPlace[1]]
-		if nextPlace == "#" {
+		for maze[wardCoordinates[0]][wardCoordinates[1]] == "#" {
+			StepBack(wardCoordinates, direction)
 			direction = DirectionChange(direction)
-			WardNextPlace[0] = wardCoordinates[0]
-			WardNextPlace[1] = wardCoordinates[1]
-			Move(WardNextPlace, direction)
+			Move(wardCoordinates, direction)
 		}
-		Move(wardCoordinates, direction)
-
+		nextMove := CopyMove(wardCoordinates, direction)
+		if isMazeInfiniteLoopWith(maze, wardCoordinates, direction, nextMove) {
+			infiniteLoopsCounter++
+			infinityMapPlaces[makeKey(nextMove[0], nextMove[1])] = true
+		}
 	}
 	return mapPlaces, infiniteLoopsCounter, infinityMapPlaces
 }
 
 // Evaluate if, with current maze, and ward position, this is a infinite loop
 func isMazeInfiniteLoopWith(maze [][]string, startPosition []int, startDirection string, blockPosition []int) bool {
-
 	position := copyPosition(startPosition)
 	direction := string(startDirection)
-	// didTurn := 0
-	nextPosition := copyPosition(position)
-	countloop := 0
-
 	traversedPaths := make(map[string]bool)
 	for WardInMap(position, len(maze)) {
-
-		Move(nextPosition, direction)
-		isValidWardInMap := WardInMap(nextPosition, len(maze))
+		Move(position, direction)
+		isValidWardInMap := WardInMap(position, len(maze))
 		if !isValidWardInMap {
 			return false
 		}
-		nextPlace := maze[nextPosition[0]][nextPosition[1]]
-		if nextPlace == "#" || (nextPosition[0] == blockPosition[0] && nextPosition[1] == blockPosition[1]) {
+		for maze[position[0]][position[1]] == "#" || (position[0] == blockPosition[0] && position[1] == blockPosition[1]) {
+			StepBack(position, direction)
 			direction = DirectionChange(direction)
-			accumulator, changes := thisDirectionChanges(maze, position, direction)
+			Move(position, direction)
+			accumulator, changes := thisDirectionChanges(maze, position, direction, blockPosition)
 			if traversedPaths[accumulator] {
 				return true
 			}
 			if changes {
 				traversedPaths[accumulator] = true
 			}
-			nextPosition = copyPosition(position)
-			Move(nextPosition, direction)
 		}
-		Move(position, direction)
-		countloop++
 	}
-	// printMap(maze, position, direction, blockPosition)
 	// fmt.Println("Bye", didTurn)
 	return false
 }
-func thisDirectionChanges(maze [][]string, position []int, direction string) (string, bool) {
+func thisDirectionChanges(maze [][]string, position []int, direction string, blockPosition []int) (string, bool) {
 	newPos := copyPosition(position)
 	accumulator := ""
+	// printMap(maze, position, direction, blockPosition)
 	for WardInMap(newPos, len(maze)) {
 		accumulator += makeKey(newPos[0], newPos[1])
-		if maze[newPos[0]][newPos[1]] == "#" {
+		if maze[newPos[0]][newPos[1]] == "#" || (newPos[0] == blockPosition[0] && newPos[1] == blockPosition[1]) {
 			return accumulator, true
 		}
 		Move(newPos, direction)
 	}
 	return accumulator, false
 }
-
 func copyPosition(position []int) []int {
 	newPos := make([]int, 2)
 	newPos[0] = position[0]
@@ -148,7 +128,32 @@ func Move(wardCoordinates []int, direction string) {
 		wardCoordinates[1]--
 	}
 }
-
+func CopyMove(wardCoordinates []int, direction string) []int {
+	newPos := make([]int, 2)
+	newPos[0] = wardCoordinates[0]
+	newPos[1] = wardCoordinates[1]
+	if direction == "top" {
+		newPos[0]--
+	} else if direction == "right" {
+		newPos[1]++
+	} else if direction == "bottom" {
+		newPos[0]++
+	} else if direction == "left" {
+		newPos[1]--
+	}
+	return newPos
+}
+func StepBack(wardCoordinates []int, direction string) {
+	if direction == "top" {
+		wardCoordinates[0]++
+	} else if direction == "right" {
+		wardCoordinates[1]--
+	} else if direction == "bottom" {
+		wardCoordinates[0]--
+	} else if direction == "left" {
+		wardCoordinates[1]++
+	}
+}
 func DirectionChange(direction string) string {
 	if direction == "top" {
 		return "right"
@@ -165,19 +170,16 @@ func DirectionChange(direction string) string {
 // Maze is square
 func WardInMap(wardCoordinates []int, maxSize int) bool {
 	return (wardCoordinates[0] < maxSize && wardCoordinates[0] >= 0) && (wardCoordinates[1] < maxSize && wardCoordinates[1] >= 0)
-
 }
 func makeKey(a int, b int) string {
 	x := strconv.Itoa(a)
 	y := strconv.Itoa(b)
-	return "[" +x + ":" + y + "]"
+	return "[" + x + ":" + y + "]"
 }
-
 func printMap(maze [][]string, wardCoordinates []int, direction string, obstacle []int) {
 	buffer := "__Maze__\n"
 	for x, char := range maze {
 		for y, char2 := range char {
-
 			if obstacle[0] == x && obstacle[1] == y {
 				buffer += "O"
 				continue
