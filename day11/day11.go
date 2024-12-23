7a427304f22b4b4d90aa0240c113c22cf37bd758
Andres Carrillo <af.carrillo@live.com>
2024-12-23 16:03:28 -0400

    40 in 7715MS

--- day11/day11.go
+++ day11/day11.go
@@ -10,36 +10,50 @@ func main() {
 	fmt.Println("Test")
 	initialStones := []int{6563348, 67, 395, 0, 6, 4425, 89567, 739318}
 	// initialStones := []int{125, 17}
-	
 	startTime := time.Now()
 	for i := 0; i < 75; i++ {
 		initialStones = blink(initialStones)
 		timed := time.Since(startTime)
-		fmt.Println(i, len(initialStones), "MS", timed.Milliseconds() )
+		fmt.Println(i, len(initialStones), "MS", timed.Milliseconds())
 	}
 	// fmt.Println(initialStones)
 	fmt.Println(len(initialStones))
 }
 
 func blink(initialstones []int) []int {
+	counters0 := 0
+	countersmod := 0
+	// countersOthers := 0
 	result := make([]int, 0)
 	for _, stone := range initialstones {
-		result = append(result, processStone(stone)...)
+		if stone == 0 {
+			counters0++
+			// result = append(result, 1)
+			continue
+		}
+		stoneLen := lenLoop10(stone)
+		if stoneLen%2 == 0 {
+			countersmod++
+			res := strconv.Itoa(stone)
+			leftPart, _ := strconv.Atoi(res[0:stoneLen/2])
+			rightPart, _ := strconv.Atoi(res[stoneLen/2:])
+			result = append(result, leftPart, rightPart)
+			continue
+		}
+		result = append(result, stone*2024)
 	}
-	return result
-}
+	appendedOnes := make([]int, counters0)
+	for idx := range appendedOnes {
+		appendedOnes[idx] = 1
+	}
+	result = append(result, appendedOnes...)
+	
 
-func processStone(a int) []int {
-	if a == 0 {
-		return []int{1}
-	}
-	if lenLoop10(a)%2 == 0 {
-		res := strconv.Itoa(a)
-		leftPart, _ := strconv.Atoi(res[0 : len(res)/2])
-		rightPart, _ := strconv.Atoi(res[len(res)/2:])
-		return []int{leftPart, rightPart}
-	}
-	return []int{a * 2024}
+
+	// fmt.Println("___________")
+	// fmt.Println(counters0, countersmod, countersOthers, "Z")
+	// fmt.Println("___________")
+	return result
 }
 
 // https://stackoverflow.com/questions/68122675/fastest-way-to-find-number-of-digits-of-an-integer
