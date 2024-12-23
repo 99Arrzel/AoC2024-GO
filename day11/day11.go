e72cecf6f188d4354f8e9ecd3fdf0daa22584bcf
Andres Carrillo <af.carrillo@live.com>
2024-12-23 16:45:01 -0400

    40 in 5821ms

--- day11/day11.go
+++ day11/day11.go
@@ -17,42 +17,53 @@ func main() {
 		fmt.Println(i, len(initialStones), "MS", timed.Milliseconds())
 	}
 	// fmt.Println(initialStones)
-	fmt.Println(len(initialStones))
+	fmt.Println(len(initialStones), initialStones)
 }
 
 func blink(initialstones []int) []int {
 	counters0 := 0
-	countersmod := 0
-	// countersOthers := 0
+	append2024Multiplies := make([]int, 0, len(initialstones))
+	appendPairValues := make([]int, 0, len(initialstones))
 	result := make([]int, 0)
-	for _, stone := range initialstones {
+	for idx, stone := range initialstones {
 		if stone == 0 {
 			counters0++
-			// result = append(result, 1)
 			continue
 		}
 		stoneLen := lenLoop10(stone)
 		if stoneLen%2 == 0 {
-			countersmod++
-			res := strconv.Itoa(stone)
-			leftPart, _ := strconv.Atoi(res[0:stoneLen/2])
-			rightPart, _ := strconv.Atoi(res[stoneLen/2:])
-			result = append(result, leftPart, rightPart)
+			appendPairValues = append(appendPairValues, idx)
+			// halfLen := stoneLen / 2
+			// res := strconv.Itoa(stone)
+			// leftPart, _ := strconv.Atoi(res[0:halfLen])
+			// rightPart, _ := strconv.Atoi(res[halfLen:])
+			// result = append(result, leftPart, rightPart)
 			continue
 		}
-		result = append(result, stone*2024)
+		// result = append(result, stone*2024)
+		append2024Multiplies = append(append2024Multiplies, idx)
 	}
 	appendedOnes := make([]int, counters0)
 	for idx := range appendedOnes {
 		appendedOnes[idx] = 1
 	}
+	for idx := range append2024Multiplies {
+		append2024Multiplies[idx] = initialstones[append2024Multiplies[idx]] * 2024
+	}
+	pairValues := make([]int, 0,len(appendPairValues)*2)
+	for idx := range appendPairValues {
+		stone := initialstones[appendPairValues[idx]]
+		stoneLen := lenLoop10(stone)
+		halfLen := stoneLen / 2
+		res := strconv.Itoa(stone)
+		leftPart, _ := strconv.Atoi(res[0:halfLen])
+		rightPart, _ := strconv.Atoi(res[halfLen:])
+		pairValues = append(pairValues, leftPart, rightPart)
+	}
+
 	result = append(result, appendedOnes...)
-	
-
-
-	// fmt.Println("___________")
-	// fmt.Println(counters0, countersmod, countersOthers, "Z")
-	// fmt.Println("___________")
+	result = append(result, append2024Multiplies...)
+	result = append(result, pairValues...)
 	return result
 }
 
