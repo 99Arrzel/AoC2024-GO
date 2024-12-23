0242082648e96744e3e5437302263b59cd92ff52
Andres Carrillo <af.carrillo@live.com>
2024-12-23 19:02:06 -0400

    WIP mejora con map

--- day11/day11.go
+++ day11/day11.go
@@ -2,69 +2,66 @@ package main
 
 import (
 	"fmt"
+	"maps"
 	"strconv"
-	"time"
 )
 
 func main() {
-	fmt.Println("Test")
-	initialStones := []int{6563348, 67, 395, 0, 6, 4425, 89567, 739318}
-	// initialStones := []int{125, 17}
-	startTime := time.Now()
-	for i := 0; i < 75; i++ {
-		initialStones = blink(initialStones)
-		timed := time.Since(startTime)
-		fmt.Println(i, len(initialStones), "MS", timed.Milliseconds())
+	// initialStones := []int{6563348, 67, 395, 0, 6, 4425, 89567, 739318}
+	initialStones := []int{125, 17}
+	mappedCounters := make(map[int]int)
+	for _, stone := range initialStones {
+		_, ok := mappedCounters[stone]
+		// mappedCounters[stone]++
+		if ok {
+			mappedCounters[stone]++
+			continue
+		}
+		mappedCounters[stone] = 1
+	}
+	// startTime := time.Now()
+	blink(initialStones)
+	for i := 0; i < 25; i++ {
+		// timed := time.Since(startTime)
+		// fmt.Println(i, len(initialStones), "MS", timed.Milliseconds())
 	}
 	// fmt.Println(initialStones)
 	fmt.Println(len(initialStones), initialStones)
 }
 
-func blink(initialstones []int) []int {
-	counters0 := 0
-	append2024Multiplies := make([]int, 0, len(initialstones))
-	appendPairValues := make([]int, 0, len(initialstones))
-	result := make([]int, 0)
-	for idx, stone := range initialstones {
-		if stone == 0 {
-			counters0++
+func blink(initialstones []int) (map[int]int, int) {
+	mappedCounters := make(map[int]int)
+	for _, stone := range initialstones {
+		_, ok := mappedCounters[stone]
+		// mappedCounters[stone]++
+		if ok {
+			mappedCounters[stone]++
 			continue
 		}
-		stoneLen := lenLoop10(stone)
-		if stoneLen%2 == 0 {
-			appendPairValues = append(appendPairValues, idx)
-			// halfLen := stoneLen / 2
-			// res := strconv.Itoa(stone)
-			// leftPart, _ := strconv.Atoi(res[0:halfLen])
-			// rightPart, _ := strconv.Atoi(res[halfLen:])
-			// result = append(result, leftPart, rightPart)
-			continue
-		}
-		// result = append(result, stone*2024)
-		append2024Multiplies = append(append2024Multiplies, idx)
-	}
-	appendedOnes := make([]int, counters0)
-	for idx := range appendedOnes {
-		appendedOnes[idx] = 1
-	}
-	for idx := range append2024Multiplies {
-		append2024Multiplies[idx] = initialstones[append2024Multiplies[idx]] * 2024
-	}
-	pairValues := make([]int, 0,len(appendPairValues)*2)
-	for idx := range appendPairValues {
-		stone := initialstones[appendPairValues[idx]]
-		stoneLen := lenLoop10(stone)
-		halfLen := stoneLen / 2
-		res := strconv.Itoa(stone)
-		leftPart, _ := strconv.Atoi(res[0:halfLen])
-		rightPart, _ := strconv.Atoi(res[halfLen:])
-		pairValues = append(pairValues, leftPart, rightPart)
+		mappedCounters[stone] = 1
 	}
 
-	result = append(result, appendedOnes...)
-	result = append(result, append2024Multiplies...)
-	result = append(result, pairValues...)
-	return result
+
+	counter := 0
+	mappedKeys := maps.Keys(mappedCounters)
+	fmt.Println("Before", mappedCounters)
+	mappedResult := make(map[int]int)
+	for key := range mappedKeys {
+		// value := mappedCounters[key]
+		newKeys := processStone(key)
+		for _, keyToAdd := range newKeys {
+			val, ok := mappedResult[keyToAdd]
+			if ok {
+				counter += val
+				mappedResult[keyToAdd]++
+				continue
+			}
+			mappedResult[keyToAdd] = 1
+			counter++
+		}
+	}
+	fmt.Println(mappedResult, counter)
+	return mappedResult, counter
 }
 
 // https://stackoverflow.com/questions/68122675/fastest-way-to-find-number-of-digits-of-an-integer
@@ -79,4 +76,18 @@ func lenLoop10(i int) int {
 		count++
 	}
 	return count
+}
+
+func processStone(stone int) []int {
+
+	if stone == 0 {
+		return []int{1}
+	}
+	res := strconv.Itoa(stone)
+	if len(res)%2 == 0 {
+		leftPart, _ := strconv.Atoi(res[0 : len(res)/2])
+		rightPart, _ := strconv.Atoi(res[len(res)/2:])
+		return []int{leftPart, rightPart}
+	}
+	return []int{stone * 2024}
 }
