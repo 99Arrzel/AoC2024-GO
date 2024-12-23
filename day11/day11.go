e3e372746a58967f44215f76ee1b2c3eed43a9b6
Andres Carrillo <af.carrillo@live.com>
2024-12-23 14:50:07 -0400

    Timed with fast len

--- day11/day11.go
+++ day11/day11.go
@@ -3,23 +3,26 @@ package main
 import (
 	"fmt"
 	"strconv"
+	"time"
 )
 
 func main() {
 	fmt.Println("Test")
-	initialStones := []int{6563348, 67, 395, 0, 6,4425,89567,739318}
+	initialStones := []int{6563348, 67, 395, 0, 6, 4425, 89567, 739318}
 	// initialStones := []int{125, 17}
 	
-	for i := 0; i< 75 ; i++{
+	startTime := time.Now()
+	for i := 0; i < 75; i++ {
 		initialStones = blink(initialStones)
-		fmt.Println(i, len(initialStones))
+		timed := time.Since(startTime)
+		fmt.Println(i, len(initialStones), "MS", timed.Milliseconds() )
 	}
-	// fmt.Println(initialStones)	
+	// fmt.Println(initialStones)
 	fmt.Println(len(initialStones))
 }
 
-func blink (initialstones []int) []int{
-	result := make([]int,0)
+func blink(initialstones []int) []int {
+	result := make([]int, 0)
 	for _, stone := range initialstones {
 		result = append(result, processStone(stone)...)
 	}
@@ -30,11 +33,25 @@ func processStone(a int) []int {
 	if a == 0 {
 		return []int{1}
 	}
-	res := strconv.Itoa(a)
-	if len(res)%2 == 0 {
+	if lenLoop10(a)%2 == 0 {
+		res := strconv.Itoa(a)
 		leftPart, _ := strconv.Atoi(res[0 : len(res)/2])
 		rightPart, _ := strconv.Atoi(res[len(res)/2:])
 		return []int{leftPart, rightPart}
 	}
 	return []int{a * 2024}
+}
+
+// https://stackoverflow.com/questions/68122675/fastest-way-to-find-number-of-digits-of-an-integer
+
+func lenLoop10(i int) int {
+	if i >= 1e18 {
+		return 19
+	}
+	x, count := 10, 1
+	for x <= i {
+		x *= 10
+		count++
+	}
+	return count
 }
